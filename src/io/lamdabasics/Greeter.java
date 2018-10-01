cf862b67abf9438acae1f1e6fd59cc5ff8618ec4
satabdi.chakraborty <satabdi.d@gmail.com>
2018-10-01 10:28:29 -0400

    First checkin

---
+++ src/io/lamdabasics/Greeter.java
@@ -0,0 +1,16 @@
+package io.lamdabasics;
+
+public class Greeter {
+	
+	private void greet() {
+		System.out.println("Hello world!");
+
+	}
+
+	public static void main(String[] args) {
+		Greeter g = new Greeter();
+		g.greet();
+
+	}
+
+}
